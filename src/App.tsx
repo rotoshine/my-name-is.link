2c988357095a48a23c9744226b7df833503fb836
rotoshine <rotoshine@gmail.com>
2020-01-02 22:01:12 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import { random } from 'lodash'
+
+interface Image {
+  src: string
+  bgColor?: string
+  isFullScreen?: boolean
 }
 
-export default App;
+const images = [
+  {
+    src: '4fdbe805909ca.jpeg',
+    bgColor: '#000000',
+  },
+  {
+    src: '5117f73dc1c0c.jpeg',
+  },
+  {
+    src: 'images.jpeg',
+    bgColor: '#0f7b49',
+  },
+  {
+    src: 'maxresdefault.jpg',
+    isFullScreen: true,
+  },
+] as Image[]
+
+const DEFAULT_BG_COLOR = '#ffffff'
+
+const App: React.FC = () => {
+  const image = images[random(0, images.length - 1)]
+
+  return (
+    <div
+      className="App"
+      style={{
+        backgroundColor: image.bgColor ? image.bgColor : DEFAULT_BG_COLOR,
+      }}
+    >
+      {image.isFullScreen ? (
+        <div
+          style={{
+            width: '100vw',
+            height: '100vh',
+            backgroundImage: `url('/images/${image.src}')`,
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+          }}
+        ></div>
+      ) : (
+        <img
+          className="App__image"
+          src={`/images/${image.src}`}
+          alt="my name is link. not zelda."
+        />
+      )}
+    </div>
+  )
+}
+
+export default App
